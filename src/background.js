75595fb76a76a7c2cb8c0710c0ee73b9158ce80e
kkiyama117 <k.kiyama117@gmail.com>
2021-03-17 20:13:28 +0900

    Keep

--- src/background.js
+++ src/background.js
@@ -14,24 +14,17 @@ chrome.app.runtime.onLaunched.addListener(function () {
     }
 
     function log(text) {
-        // $('log').value += text + '\n';
         console.log(text);
     }
 
     const port = 9999;
-    let isServer = false;
     if (http.Server && http.WebSocketServer) {
         const server = new http.Server();
         const wsServer = new http.WebSocketServer(server);
         server.listen(port);
-        isServer = true;
 
         server.addEventListener('request', function (req) {
             let url = req.headers.url;
-            if (url === '/') {
-                url = '/index.html';
-            }
-            // Serve the pages of this chrome application.
             req.serveUrl(url);
             return true;
         });
@@ -43,16 +36,12 @@ chrome.app.runtime.onLaunched.addListener(function () {
             log('Client connected');
             const socket = req.accept();
             connectedSockets.push(socket);
-
-            // When a message is received on one socket, rebroadcast it on all
-            // connected sockets.
             socket.addEventListener('message', function (e) {
                 for (let i = 0; i < connectedSockets.length; i++) {
                     connectedSockets[i].send(e.data);
                 }
             });
 
-            // When a socket is closed, remove it from the list of connected sockets.
             socket.addEventListener('close', function () {
                 log('Client disconnected');
                 for (let i = 0; i < connectedSockets.length; i++) {
